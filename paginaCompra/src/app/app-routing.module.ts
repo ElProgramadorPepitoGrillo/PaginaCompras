ddc66311ee9d8d63f11996c604c5983e3f60b993
ElProgramadorPepitoGrillo <paredes.marco.jr@gmail.com>
2022-12-26 14:16:08 -0600

    Navegacion entre componentes

--- paginaCompra/src/app/app-routing.module.ts
+++ paginaCompra/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaErrorComponent } from './Components/PaginaError/PaginaError.component';
+import { PaginaRegistroComponent } from './Components/PaginaRegistro/PaginaRegistro.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:PaginaRegistroComponent
+  },
+  {
+    path:'2',component:PaginaErrorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
