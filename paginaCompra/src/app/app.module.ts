297c873cccefa270c728aba950341a60db36e7ff
ElProgramadorPepitoGrillo <paredes.marco.jr@gmail.com>
2022-12-26 14:37:01 -0600

    Registro de componentes para el uso de directivas NG

--- paginaCompra/src/app/app.module.ts
+++ paginaCompra/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { PaginaRegistroComponent } from './Components/PaginaRegistro/PaginaRegistro.component';
+import { PaginaErrorComponent } from './Components/PaginaError/PaginaError.component';
+import { PaginaComprasComponent } from './Components/PaginaCompras/PaginaCompras.component';
+import { PaginaPagoComponent } from './Components/PaginaPago/PaginaPago.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaginaRegistroComponent,
+    PaginaErrorComponent,
+    PaginaComprasComponent,
+    PaginaPagoComponent
   ],
   imports: [
     BrowserModule,
