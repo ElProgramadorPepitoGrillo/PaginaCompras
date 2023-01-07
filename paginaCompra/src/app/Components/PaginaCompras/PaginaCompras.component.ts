b687ac87d73cb194b1e0029c2aa4d22dba384775
ElProgramadorPepitoGrillo <paredes.marco.jr@gmail.com>
2023-01-07 12:44:33 -0600

    Productos

--- paginaCompra/src/app/Components/PaginaCompras/PaginaCompras.component.ts
+++ paginaCompra/src/app/Components/PaginaCompras/PaginaCompras.component.ts
@@ -12,7 +12,17 @@ export class PaginaComprasComponent implements OnInit {
 
 
   cantidad:number=0
+  cantidad2:number=0
+  cantidad3:number=0
+  cantidad4:number=0
+  cantidad5:number=0
+  cantidad6:number=0
   total:number
+  total2:number
+  total3:number
+  total4:number
+  total5:number
+  total6:number
 
   
 
@@ -29,20 +39,20 @@ export class PaginaComprasComponent implements OnInit {
     this.router.navigate(['4'])
   }
   
-  sumar(precio:number,nombreproducto:string){
-    this.total=this.cantidad*precio
+  sumar(precio:number,nombreproducto:string,cant:number,tot:number){
+    tot=cant*precio
     console.log(this.total); 
     console.log(this.cantidad);
     
   }
-  restar(precio:number,nombreproducto:string){
-    this.total=this.cantidad*precio
-    console.log(this.total); 
-    console.log(this.cantidad);    
+  restar(precio:number,nombreproducto:string,cant:number,tot:number){
+    tot=cant*precio
+    console.log(tot); 
+    console.log(cant);    
   }
-  comprar(precio:number, nombreproducto:string){    
-    this.total=this.cantidad*precio
-    localStorage.setItem(nombreproducto,this.total.toString())
+  comprar(precio:number, nombreproducto:string,cant:number,tot:number){    
+    tot=cant*precio
+    localStorage.setItem(nombreproducto,'$'+tot.toString()+'.00')
   }
 
 
